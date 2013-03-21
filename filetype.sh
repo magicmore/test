453263c179ac8b0777b42cf510ec4bb02580ae90
magicmore <magicmore@yeah.net>
2013-03-21 11:32:25 +0800

    Init

---
+++ filetype.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+then echo $0 basepath;
+fi
+path=$1
+
+declare -A statarray;
+
+while read line
+do
+    ftype=`file -b "$line"`
+    let statarray["$ftype"]++;
+done< <(find $path -type f -print)
+
+echo =================================
+for ftype in "${!statarray[@]}"
+do 
+    echo $ftype : ${statarray["$ftype"]} 
+done
